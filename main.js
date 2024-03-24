8394d1168402121e4bb169935b05edf869b02ff0
MrPalmaBR <luizzp@msn.com>
2024-03-24 19:37:08 -0300

    Adição do main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuario == ""){
+    nomeUsuario = prompt("Qual é o seu nome?");
+};
+if(nomeUsuario == null){
+    elemento.textContent = "seja bem-vindo"
+}   else{
+    elemento.textContent = nomeUsuario;
+};
